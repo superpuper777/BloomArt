200288b9eb26fe7ed37917af5f373057108bb08c
alena.budnik <alena.budnik@opstalent.com>
2025-03-21 16:28:00 +0300

    feat: add logic for uploading a new image

--- src/components/ImageUpload.vue
+++ src/components/ImageUpload.vue
@@ -1,14 +1,36 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
+import axios from "axios";
+const emit = defineEmits();
 
-const fileName = ref('');
+const fileName = ref("");
 const fileInput = ref(null);
+const apiUrl = import.meta.env.VITE_API_URL;
 
-const uploadImage = () => {
+const uploadImage = async () => {
   const file = fileInput.value.files[0];
   if (file) {
-    console.log('Uploading file:', file.name);
-    //TODO
+    console.log("Uploading file:", file.name);
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const response = await axios.post(
+        `${apiUrl}/upload`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      console.log("File uploaded:", response.data);
+
+      // emit('closeModal');
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
   }
 };
 
@@ -27,20 +49,38 @@ const handleFileChange = () => {
         <div class="icon">
           <svg xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 24 24">
             <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
-            <g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g>
+            <g
+              stroke-linejoin="round"
+              stroke-linecap="round"
+              id="SVGRepo_tracerCarrier"
+            ></g>
             <g id="SVGRepo_iconCarrier">
-              <path fill="" d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z" clip-rule="evenodd" fill-rule="evenodd"></path>
+              <path
+                fill=""
+                d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z"
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+              ></path>
             </g>
           </svg>
         </div>
         <div class="text">
           <span>Click to upload image</span>
         </div>
-        <input type="file" id="fileInput" ref="fileInput" class="file-input" @change="handleFileChange" />
+        <input
+          type="file"
+          id="fileInput"
+          ref="fileInput"
+          class="file-input"
+          @change="handleFileChange"
+        />
       </label>
 
       <div v-if="fileName">
-        <p>File selected: <span class="file-name">{{ fileName }}</span></p>  <!-- Отображаем имя файла -->
+        <p>
+          File selected: <span class="file-name">{{ fileName }}</span>
+        </p>
+        <!-- Отображаем имя файла -->
       </div>
 
       <button type="submit" class="submit-button">Upload</button>
@@ -49,7 +89,6 @@ const handleFileChange = () => {
 </template>
 
 <style scoped>
-
 .form {
   display: flex;
   flex-direction: column;
@@ -67,7 +106,6 @@ const handleFileChange = () => {
   align-items: center;
   justify-content: center;
   border: 2px dashed #849c97;
-  /* background-color: rgba(255, 255, 255, 1); */
   padding: 1.5rem;
   border-radius: 10px;
   box-shadow: 0px 48px 35px -48px rgba(0, 0, 0, 0.1);
@@ -123,7 +161,7 @@ const handleFileChange = () => {
 }
 
 .submit-button:hover {
-  background-color: #0c5c4f;;
+  background-color: #0c5c4f;
 }
 
 .submit-button:focus {
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { defineEmits } from 'vue';
-import ImageUpload from '../components/ImageUpload.vue'
 
 const emit = defineEmits();
 const props = defineProps(['title'])
@@ -27,7 +26,6 @@ const props = defineProps(['title'])
   left: 0;
   right: 0;
   bottom: 0;
-  /* background-color: var(--vt-c-black-soft); */
   display: flex;
   justify-content: center;
   align-items: center;
